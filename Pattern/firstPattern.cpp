860c0fc4aad8c9378d8f43ec8ea1a339a32f82a1
Akash Singh <akashsingh7081178619@gmail.com>
2026-09-19 15:10:36 +0530

    Add first C++ pattern program

---
+++ Pattern/firstPattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    // Outer loop for rows
+    for (int i = 1; i <= rows; i++) {
+        // Inner loop for stars in each row
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
